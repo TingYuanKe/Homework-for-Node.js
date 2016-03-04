26500393675d0798c9e28f0408f9ba28a50250d1
TIngYuanKeke <prelude0390@hotmail.com>
2016-03-04 18:23:17 +0800

    first commit

--- wk1/app.js
+++ wk1/app.js
@@ -1,6 +1,8 @@
 var a = 1;
 var b = 2;
-console.log(a*b);
-greetYou = name=>consle.log('Hi'+name+'!');
+console.log((a*b));
+greetYou = name=>console.log('Hi'+name+'!');
 greetYou('NTUE');
 console.log('Hello World!');
+
+
